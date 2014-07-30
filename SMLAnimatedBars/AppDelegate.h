91380aa289c3fae9063b1e442ec16100958f7502
Ivan Blagajic <iblagajic@me.com>
2014-07-30 21:37:01 +0200

    Initial Commit

---
+++ SMLAnimatedBars/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SMLAnimatedBars
+//
+//  Created by Ivan Blagajić on 30/07/14.
+//  Copyright (c) 2014 Simple Things. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SMLAnimatedBars/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SMLAnimatedBars
+//
+//  Created by Ivan Blagajić on 30/07/14.
+//  Copyright (c) 2014 Simple Things. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
